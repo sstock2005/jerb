0e0fdb76694a720d7469449e24561a94c737f425
SamzyDev <144393153+sstock2005@users.noreply.github.com>
2023-09-18 13:35:41 -0500

    9/18/23
    - Fixed commands sync
    - Added super rare cat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, discord, configparser, aiohttp, io, datetime, requests
+import random, discord, configparser, aiohttp, io, datetime, requests, os
 from discord import app_commands
 from data import facts, noises
 
@@ -11,11 +11,11 @@ class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
-    async def setup_hook(self):
-        self.tree.copy_global_to(guild=ADMIN_GUILD)
-        synced = await self.tree.sync()
-        for command in synced:
-            print(command.name + " Synced!")
+    #async def setup_hook(self):
+        #self.tree.copy_global_to(guild=ADMIN_GUILD)
+        #synced = await self.tree.sync()
+        #for command in synced:
+        #    print(command.name + " Synced!")
 
 intents = discord.Intents.all()
 client = MyClient(intents=intents)
@@ -30,7 +30,14 @@ async def on_message(message):
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
-    game = discord.Game("with the cat")
+    try:
+        if os.path.getsize('rare.txt') == 0:
+            game = discord.Game("with the cat")
+        else:
+            filerare = open("rare.txt")
+            game = discord.Game("with {}".format(filerare.read()))
+    except:
+        game = discord.Game("with the cat")
     await client.change_presence(status=discord.Status.online, activity=game)
     print('------')
 
@@ -48,9 +55,18 @@ async def cat(interaction: discord.Interaction):
             img = await resp.read()
             with io.BytesIO(img) as file:
                 file=discord.File(file, "cat.png")
-                e = discord.Embed(title=requests.get("https://sillycats.me/api/noise").text, url="https://sillycats.me", color=0x07a9f3)
+                if resp.status == 418:
+                    title = "0.0001 CHANCE WTF"
+                    await client.change_presence(status=discord.Status.online, activity=discord.Game("with {}".format(interaction.user.global_name)))
+                    footer_text = "How did you get this. It's lierally 1/10000... I will remember u"
+                    filerare = open("rare.txt", 'w')
+                    filerare.write(interaction.user.global_name)
+                else:
+                    title = requests.get("https://sillycats.me/api/noise").text
+                    footer_text = "Made by Sam Stockstrom"
+                e = discord.Embed(title=title, url="https://sillycats.me", color=0x07a9f3)
                 e.set_image(url="attachment://cat.png")
-                e.set_footer(text="Made by Sam Stockstrom", icon_url="https://avatars.githubusercontent.com/u/144393153")
+                e.set_footer(text=footer_text, icon_url="https://avatars.githubusercontent.com/u/144393153")
                 e.timestamp = datetime.datetime.utcnow()
                 await interaction.response.send_message(file=file, embed=e)
 
